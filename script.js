27e6bd6bec5118313975d8b9243dfdc755fb0a25
matheus <mathdiogenes@gmail.com>
2022-07-13 17:06:20 -0300

    Requisito 17, 18 e 19

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const loginButton = document.getElementById('loginButton');
 const emailInput = document.getElementById('emailInput');
 const senhaInput = document.getElementById('senhaInput');
+const agreement = document.getElementById('agreement');
+const submitButton = document.getElementById('submit-btn');
 
 loginButton.addEventListener('click', () => {
   if (emailInput.value === 'tryber@teste.com' && senhaInput.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+agreement.addEventListener('click', () => {
+  if (agreement.checked === true) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
   }
 });
